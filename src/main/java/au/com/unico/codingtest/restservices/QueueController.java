6e371b6e341ef895bc19b02797fc5b5a49e43796
Vijay Ganesh <vijay@trueinfluence.in>
2017-05-24 15:08:32 +1000

    Initial commit with just the structure

---
+++ src/main/java/au/com/unico/codingtest/restservices/QueueController.java
@@ -0,0 +1,10 @@
+package au.com.unico.codingtest.restservices;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/queue")
+public class QueueController {
+
+}
